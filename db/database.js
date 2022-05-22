6fa95c354e9738061d51a7c82bb0e132d95455c9
Nasa28 <kaluchinasaagu@gmail.com>
2022-05-22 14:51:02 +0100

    Modify Heroku connection

--- db/database.js
+++ db/database.js
@@ -3,5 +3,9 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  ssl: true,
+  dialectOptions: {
+  ssl: true,
+  },
 });
 module.exports = sequelize;
